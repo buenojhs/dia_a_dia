685dbe81b9ebcc9a9ac40c8ce1ba7341abe1e77f
buenojhs@gmail.com <buenojhs@gmail.com>
2025-07-09 09:49:27 -0300

    for e range

--- boson_curso/for.py
+++ boson_curso/for.py
@@ -6,6 +6,18 @@
 #for numero in range(1, 11):
 #    print(numero)
 
-nome = input('Digite seu nome: ')
-for x in range(10):
-    print(f'{x+1} {nome}')
+#nome = input('Digite seu nome: ')
+#for x in range(10):
+#    print(f'{x+1} {nome}')
+
+# range (valor_inicial, valor_final, incremento)
+
+#for x in range(2, 20, 3):
+#    print(x)
+
+pedras = ('rubi', 'esmeralda', 'diamante', 'safira', 'topázio', 'ágata')
+
+for pedra in pedras:
+    if pedra == 'safira':
+        continue
+    print(pedra)
