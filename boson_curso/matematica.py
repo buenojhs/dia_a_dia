462a5a55d1ff9e8060dbb402a3da0a4012ea0431
buenojhs@gmail.com <buenojhs@gmail.com>
2025-07-14 18:46:08 -0300

    funcoes matematicas do math

---
+++ boson_curso/matematica.py
@@ -0,0 +1,11 @@
+#funcoes internas e via modulo
+
+valores = [2,5,8,-1,0,11,7,-6]
+print(max(valores)) #maior valor
+print(min(valores)) #menor valor
+a = -5
+b = 4
+print(abs(a)) #valor absoluto
+print(pow(a, b)) #potência
+c = 2.789011
+print(round(c, 3)) #arredondamento
---
+++ boson_curso/matematica2.py
@@ -0,0 +1,17 @@
+import math
+
+x = 8
+y = 100
+
+raiz_quadrada = math.sqrt(x)  # Raiz quadrada de x
+print(raiz_quadrada)
+print(math.ceil(raiz_quadrada))  # Arredondamento para cima
+print(math.floor(raiz_quadrada))  # Arredondamento para baixo
+print(round(raiz_quadrada, 2))  # Arredondamento com duas casas decimais
+
+logaritmo = math.log10(y)  # Logaritmo natural de y
+print(logaritmo)
+
+print(math.pi)  # Valor de pi
+print(math.factorial(x))  # Fatorial de x
+print(x / math.inf) # Divisão por infinito
