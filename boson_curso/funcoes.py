f27d3364c8ae1d4bfc0b549c07d9c26907cee5d3
buenojhs@gmail.com <buenojhs@gmail.com>
2025-07-21 18:40:55 -0300

    atualizacoes

---
+++ boson_curso/funcoes.py
@@ -0,0 +1,29 @@
+# Funções
+# criar um codigo modular, onde pode haver um reuso de código
+
+#def <nome_funcao> ([argumentos]):
+#    <instricoes>
+
+#def mensagem():
+#    print('Bóson Treinamentos em tecnologia')
+#    print('Curso Completo de Python.')
+
+#mensagem()
+
+#funcao com argumentos
+
+#def soma(a, b):
+#    print(a+b)
+
+#soma(12, 7)
+
+#def multi(x, y):
+    return x * y
+
+#a = 5
+#b = 8
+#c = multi(a, b)
+#print(f'O produto de {a} e {b} é {c}')
+
+def div(k, j):
+    
