ae14b2032f1348fa58ea2f96e0f2e2772530c5c9
buenojhs@gmail.com <buenojhs@gmail.com>
2025-07-28 19:26:16 -0300

    coisas

---
+++ boson_curso/programacao_orientada_objetos.py
@@ -0,0 +1,8 @@
+# orientação a objetos: paradigma de programação
+# classes (modelos abstratos que vão representar itens do mundo real) e objetos (ocorrências dessas classes)
+
+class Veiculo: 
+    def movimentar(self):
+        print(f'Sou um veículo e me desloco!')
+
+    def __init__(self)
