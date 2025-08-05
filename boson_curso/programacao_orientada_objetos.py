ac271f6816534cb49bfccac341594bae8b0f4df6
buenojhs@gmail.com <buenojhs@gmail.com>
2025-08-05 14:43:45 -0300

    poo

--- boson_curso/programacao_orientada_objetos.py
+++ boson_curso/programacao_orientada_objetos.py
@@ -5,6 +5,71 @@ class Veiculo:
     def movimentar(self):
         print(f'Sou um veículo e me desloco!')
 
-    def __init__(self)
+    def __init__(self, fabricante, modelo):
+        self.__fabricante = fabricante #colocando __ antes do nome do atributo, ele se torna privado
+        self.__modelo = modelo
+        self.__num_registro = None
+
+    #setter
+    def set_num_registro(self, registro):
+        self.__num_registro = registro    
+
+    #getter
+
+    def get_fabr_modelo(self):
+        print(f'Modelo: {self.__modelo}, Fabricante: {self.__fabricante}.\n')
+    def get_num_registro(self):
+        return self.__num_registro
+
+class Carro(Veiculo):
+    # metodo __init__ será herdado
+    def movimentar(self):
+        print(f'Sou um carro e ando pelas ruas!')
+
+class Motocicleta(Veiculo):
+    def movimentar(self):
+        print(f'Corro Muito!')
+
+class Aviao(Veiculo):
+    def __init__(self, fabricante, modelo, categoria):
+        self.__cat = categoria
+        super().__init__(fabricante, modelo)
+
+    def get_categoria(self):
+        return self.__cat            
+    def movimentar(self):
+        print(f'Sou um avião e voo pelos céus!')
+
+if __name__ == '__main__':
+    meu_veiculo = Veiculo('GM', 'Cadillac Escalade')
+    meu_veiculo.movimentar()      
+    meu_veiculo.get_fabr_modelo()
+    meu_veiculo.set_num_registro('123456789')
+    print(f'Registro: {meu_veiculo.get_num_registro()}\n')
+
+    meu_carro = Carro('Volkswagen', 'Polo')
+    meu_carro.movimentar()
+    meu_carro.get_fabr_modelo()
+
+    seu_carro = Carro('Audi', 'A5 Sportback')
+    seu_carro.movimentar()
+    seu_carro.get_fabr_modelo()
+
+    moto = Motocicleta('Honda', 'Pop 110i')
+    moto.movimentar()
+    moto.get_fabr_modelo()
+
+    meu_aviao = Aviao('Boeing', '747', 'Comercial')
+    meu_aviao.movimentar()
+    meu_aviao.get_fabr_modelo()
+    print(f'Categoria: {meu_aviao.get_categoria()}')
+#com uma classe eu posso criar multiplos objetos distintos entre si, com seus proprios dados e estruturas e interagir com eles separadamente
+
+
+        
+
+
+
+
 
 
